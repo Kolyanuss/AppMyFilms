96fa606431ebc19dfa67aa1dffd1ea6f5667987f
0xKolyanus <mukola.1zt.@gmail.com>
2021-09-30 11:05:01 +0300

    add back my files

---
+++ AppMyFilm.DAL/Interfaces/SQLInterfaces/ISQLRepositories/ISQLFilmsRepository.cs
@@ -0,0 +1,11 @@
+﻿using SkillAppAdoDapperWebApi.DAL.Entities.SQLEntities;
+using SkillManagement.DataAccess.Entities.SQLEntities;
+using SkillManagement.DataAccess.Interfaces;
+
+namespace SkillAppAdoDapperWebApi.DAL.Interfaces.SQLInterfaces.ISQLRepositories
+{
+    public interface ISQLFilmsRepository : IGenericRepository<SQLFilms, long>
+    {
+
+    }
+}
---
+++ AppMyFilm.DAL/Repositories/SQL Repositories/SQLFilmsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using SkillManagement.DataAccess.Core;
+using SkillManagement.DataAccess.Entities.SQLEntities;
+using SkillManagement.DataAccess.Interfaces.SQLInterfaces.ISQLRepositories;
+using SkillManagement.DataAccess.Interfaces;
+
+namespace SkillAppAdoDapperWebApi.DAL.Repositories.SQL_Repositories
+{
+    public class SQLFilmsRepository : GenericRepository<SQLEmployee, long>, ISQLEmployeeRepository
+    {
+        private static readonly string _tableName = "Films";
+        private static readonly bool _isSoftDelete = true;
+        public SQLFilmsRepository(IConnectionFactory connectionFactory, IConfiguration config) : base(connectionFactory, _tableName, _isSoftDelete)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection2");
+            connectionFactory.SetConnection(connectionString);
+        }
+    }
+}
