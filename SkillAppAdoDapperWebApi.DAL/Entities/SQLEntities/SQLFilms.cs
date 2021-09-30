78ec8438c865c0f67d05c0d400d28e12f87d48dd
0xKolyanus <mukola.1zt.@gmail.com>
2021-09-30 09:49:43 +0300

    add my entitie

---
+++ SkillAppAdoDapperWebApi.DAL/Entities/SQLEntities/SQLFilms.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.OData.Edm;
+using SkillManagement.DataAccess.Interfaces;
+
+namespace SkillAppAdoDapperWebApi.DAL.Entities.SQLEntities
+{
+    public class SQLFilms : IEntity<long>
+    {
+        public long Id { get; set; }
+        public string NameFilm { get; set; }
+        public Date ReleaseData { get; set; }
+        public string Country { get; set; }
+        public long DescriptionId { get; set; }
+    }
+}
